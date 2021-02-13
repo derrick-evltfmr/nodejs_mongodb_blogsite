f9abea7ada128fa374113a771e08bbe7bd98cfae
derrick-evltfmr <derrick.evltfmr@gmail.com>
2021-02-12 18:04:05 -0600

    Review NodeJS basic - request url and status

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const http = require('http')
 
 const server = http.createServer((request, response)=>{
-    console.log(request.url)
-    response.end("HELLO NODE JS")
+    if (request.url === '/about'){
+        response.end("THE ABOUT PAGE")
+    }
+    else if (request.url === '/contact') {
+        response.end("THE CONTACT PAGE")
+    }
+    else if (request.url === '/'){
+        response.end("THE HOME PAGE")
+    }
+    else {
+        response.writeHead(404)
+        response.end("PAGE NOT FOUND")
+    }
+    
 })
 
 server.listen(3000)
