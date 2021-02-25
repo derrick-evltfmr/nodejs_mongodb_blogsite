8c02183a7fb09ef7f845c11a4299947280640fb9
derrick-evltfmr <derrick.evltfmr@gmail.com>
2021-02-24 21:10:26 -0600

    Review NodeJS - Express basics

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
-const http = require('http')
-const fs = require('fs')
+const express = require('express')
 
-// synchronous means blocking code
-// so that we can make sure that the pages are read before
-// the server respond
-const aboutPage = fs.readFileSync('about.html') 
-const contactPage = fs.readFileSync('contact.html')
-const homePage = fs.readFileSync('index.html')
+const app = express()
 
-const server = http.createServer((request, response)=>{
-    if (request.url === '/about'){
-        response.end(aboutPage)
-    }
-    else if (request.url === '/contact') {
-        response.end(contactPage)
-    }
-    else if (request.url === '/'){
-        response.end(homePage)
-    }
-    else {
-        response.writeHead(404)
-        response.end("PAGE NOT FOUND")
-    }
-    
+app.get('/', (request, response)=>{
+    response.json({
+        name: "Derrick",
+        page: "Home page",
+        response_type: "json"
+    })
 })
 
-server.listen(3000)
+app.get('/about', (request, response)=>{
+    response.send({
+        name: "Derrick",
+        page: "About page",
+        response_type: "send"
+    })
+})
+
+app.listen(3000, ()=>{
+    console.log('App listening on port 3000')
+})
+
+// const http = require('http')
+// const fs = require('fs')
+
+// // synchronous means blocking code
+// // so that we can make sure that the pages are read before
+// // the server respond
+// const aboutPage = fs.readFileSync('about.html') 
+// const contactPage = fs.readFileSync('contact.html')
+// const homePage = fs.readFileSync('index.html')
+
+// const server = http.createServer((request, response)=>{
+//     if (request.url === '/about'){
+//         response.end(aboutPage)
+//     }
+//     else if (request.url === '/contact') {
+//         response.end(contactPage)
+//     }
+//     else if (request.url === '/'){
+//         response.end(homePage)
+//     }
+//     else {
+//         response.writeHead(404)
+//         response.end("PAGE NOT FOUND")
+//     }
+    
+// })
+
+// server.listen(3000)
