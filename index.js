26c10716b6207a9ff5a627156461f805e0fd16b6
derrick-evltfmr <derrick.evltfmr@gmail.com>
2021-02-27 13:42:03 -0600

    Review NodeJS _ static asset with express

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.static('public'))
+
 app.get('/', (req, res)=>{
 
     res.sendFile(path.resolve(__dirname,'index.html'))
