4cdcd74b88e2228ffce0e0e64895c8ef1ca470cc
derrick-evltfmr <derrick.evltfmr@gmail.com>
2021-03-06 15:52:09 -0600

    Register home route, Restructure project

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+})
 
 app.listen(4000, ()=>{
     console.log("APP running on port 4000")
