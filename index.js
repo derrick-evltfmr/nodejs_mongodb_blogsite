b2d3e9d68107bccc15e6d322b64e8442b83078e1
derrick-evltfmr <derrick.evltfmr@gmail.com>
2021-03-07 13:31:02 -0600

    Add about page and additional index.html route

--- index.js
+++ index.js
@@ -5,9 +5,26 @@ const app = express()
 
 app.use(express.static('public'))
 
+// Home Page
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'pages/index.html'))
 })
+
+app.get('/index.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+})
+
+
+// About Page
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+})
+
+app.get('/about.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+})
+
+
 
 app.listen(4000, ()=>{
     console.log("APP running on port 4000")
